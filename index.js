969ac8e7f1d4b540c5894963b9d57f7f61b522c2
Asim Kh <asimkh@hotmail.com>
2016-12-27 11:28:44 +0400

    update package

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(app.get('port'), function () {
   console.log('app listening on port 3000!')
 })
+
+
